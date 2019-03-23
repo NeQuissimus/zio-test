b548595caf24aadb7bc5cb8e9e14c5dcb0f42e21
Tim Steinbach <tim@nequissimus.com>
2019-03-23 11:18:34 -0400

    Format a little bit

--- core/shared/src/main/scala/framework.scala
+++ core/shared/src/main/scala/framework.scala
@@ -4,24 +4,16 @@ import sbt.testing.{ Fingerprint, Framework, SubclassFingerprint }
 
 import scalaz.zio.{ Runtime, Task }
 
-trait TestSpec extends Runtime[Any] { self: Singleton  =>
+trait TestSpec extends Runtime[Any] { self: Singleton =>
   def tests: Map[String, Task[Result]]
 }
 
-trait Matchers {
-  def equals[A](expected: A, got: A) = if (expected == got) Succeeded else Failed(s"Expected ${expected}, got ${got}")
-  def expect[A](expected: A)(got: A) = equals(expected, got)
-}
-
 sealed abstract class Result
-case object Succeeded extends Result
+case object Succeeded                      extends Result
 final case class Failed(reason: Throwable) extends Result
-object Failed {
-  def apply(s: String) = new Failed(new Exception(s))
-}
 
 final class TestFramework extends Framework {
-  override val name = "ZIO-Test"
+  override val name = s"${Console.UNDERLINED}ZIO-Test${Console.RESET}"
 
   val fingerprints: Array[Fingerprint] = Array(
     new SubclassFingerprint {
--- test/shared/src/test/scala/TestSpec.scala
+++ test/shared/src/test/scala/TestSpec.scala
@@ -5,19 +5,24 @@ import scalaz.zio.test._
 
 object Bla {
   def helloWorld = ZIO.succeed("Hello World")
+  def foobar     = ZIO.fail(new Exception("This is no good"))
 }
 
-object BlaSpec extends TestSpec with DefaultRuntime with Matchers {
+object BlaSpec extends TestSpec with DefaultRuntime with SimpleMatchers {
   val tests = Map(
-    "first test" -> first,
-    "second test" -> second
+    "first test"  -> first,
+    "second test" -> second,
+    "third test"  -> third
   )
 
-  def first: Task[Result] = {
+  def first: Task[Result] =
     Bla.helloWorld.map(expect("Hello World"))
-  }
 
-  def second: Task[Result] = {
+  def second: Task[Result] =
     Bla.helloWorld.map(expect("FooBar"))
-  }
+
+  def third: Task[Result] =
+    Bla.foobar.mapError { e =>
+      compare[Throwable]("Did not receive the expected error")(a => a.getMessage == e.getMessage)(e)
+    }.flip
 }
